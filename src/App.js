5df02f57b0a77717f927240470c67a1ab66e9cf4
1Markish <mark.kipchumba@student.moringaschool.com>
2023-10-30 09:20:16 +0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import SignUp from './components/SignUp'
+//import LogIn from './components/LogIn'
+
+function App() {
+  return (
+    <div>
+      <SignUp/>
+    </div>
+  );
+}
+
+export default App;
